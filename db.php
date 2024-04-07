96e8c0eba741041bd57fe0410161c3d891cdec7b
david <dmhernandezt0@gmail.com>
2024-04-06 17:04:48 -0700

    close db connection, populate db data

--- db.php
+++ db.php
@@ -9,17 +9,18 @@
     $username = $_POST['name'];
     $comment = $_POST['comment'];
   
-    $sql = "INSERT INTO UserEntry
+    $sql = "INSERT INTO UserEntry (Name, Comment)
     VALUES('$username','$comment')";
     
     $result = $conn->query($sql);
+    $conn->close();
 
     if(!$result) {
       die("Uh-oh, there was an error adding your comment". $conn->error);
     }
     else {
       echo($username . " Your Comment was succesfully added! Returning to main page.");
-      header('Refresh: 2, URL=name_form.html');
+      header('Refresh: 2, URL=name_form.php');
     }
-    
+
 ?>
---
+++ name_form.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<style>
+  
+  h1 {
+    text-align: center top;
+    padding-top: 8%;
+    font-size: 40px;
+    font-family: 'Times New Roman', Times, serif;
+  }
+  body {
+    background-color: gray;
+    background-image: url(notebook.jpg);
+    background-position: center top;
+    background-attachment: scroll;
+    background-size: cover;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
+
+<body>
+  <h1>Submit A Comment</h1>
+  <h2>Self teaching myself HTML, CSS, PHP with MySQL.<br>Leave a comment to help me with database manipulation.</h2>
+      <form action="/db.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" maxlength="20"><br><br>
+        <textarea row="4" cols="50" maxlength="200" name="comment" id="comment" placeholder="Enter a comment..."></textarea><br>
+        <input type="submit" value="Post"><br><br>
+      </form>
+
+      <table id="myTable" name="myTable">
+      </table>
+</body>
+
+<?php
+      $servername = "";
+      $username = "";
+      $password = "";
+      $dbname = "";
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+  $sql = "SELECT * FROM UserEntry ORDER BY Id DESC";
+
+  $result = $conn->query($sql);
+
+  while ($row = $result->fetch_assoc()){
+    echo "Name: " . $row["Name"] . "<br>" . "Comment: " . $row["Comment"] ."<br><br>";
+  }
+
+  $conn->close();
+
+  ?>
+
+</html>
