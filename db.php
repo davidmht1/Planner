12c194c7bade031d78a906c05bcd6ad14c450aef
david <dmhernandezt0@gmail.com>
2024-04-13 14:44:15 -0700

    new divs and background

--- db.php
+++ db.php
@@ -1,26 +1,26 @@
 <?php
 
-    $servername = "";
-    $username = "";
-    $password = "";
-    $dbname = "";
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$servername = "";
+$username = "admin";
+$password = "";
+$dbname = "main-aws-db";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $username = $_POST['name'];
-    $comment = $_POST['comment'];
-  
-    $sql = "INSERT INTO UserEntry (Name, Comment)
-    VALUES('$username','$comment')";
-    
-    $result = $conn->query($sql);
-    $conn->close();
+$username = $_POST['name'];
+$comment = $_POST['comment'];
 
-    if(!$result) {
-      die("Uh-oh, there was an error adding your comment". $conn->error);
-    }
-    else {
-      echo($username . " Your Comment was succesfully added! Returning to main page.");
-      header('Refresh: 2, URL=name_form.php');
-    }
+$sql = "INSERT INTO UserEntry (Name, Comment)
+VALUES('$username','$comment')";
+
+$result = $conn->query($sql);
+$conn->close();
+
+if (!$result) {
+  die("Uh-oh, there was an error adding your comment" . $conn->error);
+} 
+else {
+  echo ($username . " Your Comment was succesfully added! Returning to main page.");
+  header('Refresh: 2, URL=name_form.php');
+}
 
 ?>
--- name_form.php
+++ name_form.php
@@ -1,70 +1,103 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+  <title>David's Website</title>
+</head>
 <style>
-  
-  h1 {
-    text-align: center top;
-    padding-top: 8%;
-    font-size: 40px;
-    font-family: 'Times New Roman', Times, serif;
+  .title,
+  form {
+    margin: 70px;
   }
-  body {
-    background-color: gray;
-    background-image: url(notebook.jpg);
-    background-position: center top;
-    background-attachment: scroll;
-    background-size: cover;
+
+  .title {
+    font-size: 30px
+  }
+
+  .container {
+    position: relative;
     text-align: center;
-    margin-left: auto;
-    margin-right: auto;
+    text-shadow: 5px;
+    font-size: 30px;
+    height: 500px;
+    background-size: 1500px;
+    background-position: center;
+    background-image: url(minimalist_background.jpg);
+    
+  }
+
+  .centered {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 </style>
 
 <body>
-  <h1>Submit A Comment</h1>
-  <h2>Self teaching myself HTML, CSS, PHP with MySQL.<br>Leave a comment to help me with database manipulation.</h2>
-      <form action="/db.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" maxlength="20"><br><br>
-        <textarea row="4" cols="50" maxlength="200" name="comment" id="comment" placeholder="Enter a comment..."></textarea><br>
-        <input type="submit" value="Post"><br><br>
-      </form>
+  <div class="title">
+    <p>Sample Text</p>
+  </div>
 
-      <table id="myTable" name="myTable">
-      </table>
+  <div class="container">
+    <div class="centered">Sample Text</div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" maxlength="20"><br><br>
+    <textarea row="4" cols="50" maxlength="20" name="comment" id="comment"
+      placeholder="Enter a comment..."></textarea><br>
+    <input type="submit" value="Post"><br><br>
+  </form>
+  </div>
+
+  <table id="myTable" name="myTable">
+  </table>
 </body>
 
 <?php
-      $servername = "";
-      $username = "";
-      $password = "";
-      $dbname = "";
-      $conn = new mysqli($servername, $username, $password, $dbname);
+$servername = "";
+$username = "admin";
+$password = "";
+$dbname = "main-aws-db";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-  $sql = "SELECT * FROM UserEntry ORDER BY Id DESC";
+$sql = "SELECT * FROM UserEntry ORDER BY Id DESC";
 
-  $result = $conn->query($sql);
+$result = $conn->query($sql);
 
-  $conn->close();
+// Create the HTML table
+echo '<table>';
+echo '<tr><th>User</th><th>Comment</th></tr>';
+
+while ($row = mysqli_fetch_array($result)) {
+    echo '<tr>';
+    echo '<td>' . $row['Name'] . '</td>';
+    echo '<td width=400px>' . $row['Comment'] . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
+
+$conn->close();
 ?>
 
 <script>
-
-while ($row = $result->fetch_assoc()){
-  var table = document.getElementById("myTable");
-  var row = document.createElement("tr");
-
-  var userName = document.createElement("td");
-  userName.textContent = $row['Name'];
-  row.appendChild(userName);
-
-  var comment = document.createElement("td");
-  comment.textContent = $row['Comment'];
-  row.appendChild(comment);
-
-  table.appendChild(row);
-}
-  </script>
+  $(document).ready(function () {
+    $('#myForm').submit(function (e) {
+      e.preventDefault(); // prevent default form submission
+      $.ajax({
+        url: '/Planner/db.php',
+        type: 'post',
+        data: $('#myForm').serialize(),
+        success: function () {
+          window.location.reload();
+        }
+      });
+    });
+  });
+</script>
 
 </html>
